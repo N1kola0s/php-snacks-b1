69b89adca87a358dd6d662bf50790186163aee3b
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-05-20 17:23:02 +0200

    add snack4

--- snack4/index.php
+++ snack4/index.php
@@ -1,17 +1,26 @@
 <!-- ## Snack 4
 Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
 
-<?php ?>
+<?php
+//dichiaro un array vuoto
+$array = [];
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Snack</title>
-</head>
-<body>
-    
-</body>
-</html>
+//imposto un ciclo for
+for($i=0; count($array) < 15; $i++){
+
+    //dichiaro variabile in cui inserisco dei numeri randomici da 1 a 100
+
+    $number = rand(1, 100);
+
+    //imposto condizione in cui verifico se il numero non è presente nell'array e in caso positivo lo pusho
+     if(!in_array($number, $array)){
+
+        array_push($array, $number);
+
+    }
+}
+
+//dumpo il contenuto dell'array
+    var_dump($array);
+
+?>
