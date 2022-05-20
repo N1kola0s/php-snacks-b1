8436570ebe78438ef7cb4fa884462ed13264102f
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-05-20 18:57:01 +0200

    add snack7

--- snack7/index.php
+++ snack7/index.php
@@ -8,39 +8,20 @@ $classe = [//primo livello
     [//secondi livelli
         "nome" => "Giulia",
         "cognome" => "Migliore",
-
-        "voti" => [
-            "matematica" => 8,
-            "italiano" => 7,
-            "informatica" => 6
-        ]
+        "voti" => [8,6,7]
         
     ],
     [
         "nome" => "Luca",
         "cognome" => "Militello",
-
-        "voti" => [
-
-            "matematica" => 6,
-            "italiano" => 7,
-            "informatica" => 4
-
-        ]
+        "voti" => [6, 7 , 4]
 
     ],
     [
 
         "nome" => "Andrea",
         "cognome" => "Loffredo",
-
-        "voti" => [
-
-            "matematica" => 5,
-            "italiano" => 8,
-            "informatica" => 6
-
-        ]
+        "voti" => [5, 8, 6]
 
 
     ],
@@ -49,26 +30,14 @@ $classe = [//primo livello
 
             "nome" => "Francesco",
             "cognome" => "Nocera",
-    
-            "voti" => [
-    
-                "matematica" => 9,
-                "italiano" => 6,
-                "informatica" => 7
-    
-            ]
+            "voti" => [9,6,7]
     
     
             ]
 
      ];
-
+//dumbo a schermo la struttura dati
      var_dump($classe);
-
-
-
-
-
 
 ?>
 
@@ -79,9 +48,26 @@ $classe = [//primo livello
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Snack7</title>
 </head>
 <body>
+     <!-- stampo a schermo la pagella della classe -->
+
+     <h3>Pagella della classe:</h3>
+
+    <ul>
+    <!-- imposto un ciclo for in modo da stampare le singole pagelle degli alunni -->
+    <?php for($i=0; $i <= count($classe) - 1; $i++): ?>
+
+        <!-- dichiaro una variabile dove inserisco la media voto degli alunni -->
+        <?php $average_value = array_sum($classe[$i]["voti"]) / count($classe[$i]["voti"])?>
+
+        <!-- stampo su schermo nome, cognome e media voto di ciascun alunno -->
+        <li><?php echo $classe[$i]["nome"]. ' ' .$classe[$i]["cognome"]. ' -  media voto: '. $average_value; ?></li>
+    
+    <?php endfor; ?>
+
+    </ul>
     
 </body>
 </html>
